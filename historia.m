5c22c07640ee8791f861d4d0de4ebdf232a97848
darwin quiroz <dd@pl.com>
2019-09-08 20:06:07 -0400

    este tipo habla mucho

--- historia.m
+++ historia.m
@@ -4,4 +4,4 @@ darwin tiene 18 anos me quivoque
 developmen dededededede
 eeste un archivo para jugar con git.
 esta ancioso
-ahora ghablamos de su vida
+ahora ghablamos de su vida carajter que hay que cambiar todo
